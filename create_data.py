ef54df97d51f9c1e4d5ff6fb3943b5ec8f5df027
Hannah Richert <hannah1.r@web.de>
2022-08-17 10:40:51 +0100

    changing args input method
    the config file must now be specified at  cmd line

--- create_data.py
+++ create_data.py
@@ -1,5 +1,20 @@
 from scripts.data_creation_utils import load_args,load_sample_and_save
+import argparse
+
 
 if __name__ == '__main__':
-    args = load_args('configs/data_creation.yaml')
+
+    parser = argparse.ArgumentParser(
+        description='Specify the path to your config file.')
+    parser.add_argument('-config', type=str,
+                        help="Path to where your config yaml file is stored.")
+
+    args = parser.parse_args()
+
+    try:
+        args = load_args(f'configs/{args.config}.yaml')
+    except:
+        print(f'File does not exist: configs/{args.config}.yaml')
+        args = load_args('configs/data_creation.yaml')
+
     load_sample_and_save(**args)
--- pipeline.py
+++ pipeline.py
@@ -3,6 +3,6 @@ import os
 import sys
 if __name__ == "__main__":
     print("prepare data")
-    os.system(f'python create_data.py')
+    os.system(f'python create_data.py -config data_creation')
     print("model classification")
-    os.system(f'python classification.py')
+    os.system(f'python classification.py -config classification')
