5f308685e1873b5248e840763be3c8211554e1e8
Hannah Richert <hannah1.r@web.de>
2022-09-21 15:50:32 +0100

    commenting, structure changes

--- pipeline.py
+++
@@ -1,8 +0,0 @@
-
-import os
-import sys
-if __name__ == "__main__":
-    print("prepare data")
-    os.system(f'python create_data.py -config data_creation')
-    print("model classification")
-    os.system(f'python classification.py -config classification')
